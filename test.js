6f70702404e16676db61fd3e32039d2b466f8fd0
j2noo <wlsdndml213@naver.com>
2023-07-01 12:21:36 +0900

    push upstream test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 "use strict";
-const player = { name: 's' };
+const player = { name: "s" };
 const obj = {
-    name: "jinwoo",
-    age: 21
+  name: "jinwoo",
+  age: 212,
 };
 console.log(obj.age);
