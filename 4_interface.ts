15aa4806ef9a7c67e56db738e924f6d9898a0714
j2noo <wlsdndml213@naver.com>
2023-07-02 23:46:21 +0900

    인터페이스 완료

--- 4_interface.ts
+++ 4_interface.ts
@@ -1,15 +1,38 @@
-interface User {
-  name : string
+//추상클래스 user
+abstract class User {
+  constructor(protected firstName: string, protected lastName: string) {}
+
+  abstract sayHi(name: string): string;
+  abstract fullName(): string;
 }
 
-interface Player extends User {
-
+class Player extends User {
+  sayHi(name: string): string {
+    return `say hi to ${name}`;
+  }
+  fullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
 }
 
-const nico : Player = {
-  name : "nico"
+const nico: Player = new Player("nico", "las");
+console.log(nico.sayHi("jinoo"));
+
+///
+
+interface User2 {
+  firstName: string;
+  lastName: string;
+  sayHi(name: string): string;
+  fullName(): string;
 }
 
-interface User {
-  name : string
+class Player2 implements User2 {
+  constructor(public firstName: string, public lastName: string) {}
+  public sayHi(name: string): string {
+    return `say hi to ${name}`;
+  }
+  public fullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
 }
